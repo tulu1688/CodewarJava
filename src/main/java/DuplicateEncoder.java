b0778f400a9c3d1a76817bec48fdfdefb392ac51
Tran Khai <khai.tran@ascendcorp.com>
2017-09-11 14:58:31 +0700

    Complete kata "duplicate encoder"

---
+++ src/main/java/DuplicateEncoder.java
@@ -0,0 +1,22 @@
+import java.util.stream.Collectors;
+
+/**
+ * Created by trankhai on 9/11/17.
+ */
+public class DuplicateEncoder {
+    static String encode(final String word){
+        String lowerWord = word.toLowerCase();
+        return lowerWord.chars()
+                .mapToObj(ch -> (char) ch)
+                .map(ch -> isMultipleOccur(lowerWord, ch) ? ')' : '(')
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    static boolean isMultipleOccur(String str, int c) {
+        int firstOccurs = str.indexOf(c);
+        if (firstOccurs < 0)
+            return false;
+        return str.indexOf(c, firstOccurs + 1) >= 0;
+    }
+}
---
+++ src/test/java/DuplicateEncoderTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class DuplicateEncoderTest {
+    @Test
+    public void test() {
+        assertEquals(")()())()(()()(",
+                DuplicateEncoder.encode("Prespecialized"));
+        assertEquals("))))())))",DuplicateEncoder.encode("   ()(   "));
+    }
+
+    @Test
+    public void testMultipleCharacterOccur(){
+        assertEquals(true, DuplicateEncoder.isMultipleOccur("hahaha", 'a'));
+        assertEquals(false, DuplicateEncoder.isMultipleOccur("khai", 'a'));
+    }
+}
